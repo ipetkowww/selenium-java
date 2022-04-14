1166fafbca3218effbbed94a99a44417d591fbc5
Ivan Petkov <petkow.iv@gmail.com>
2022-04-14 10:53:52 +0300

    create first class and initialize chrome driver

---
+++ selenium-java/src/test/java/base/BaseTests.java
@@ -0,0 +1,22 @@
+package base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTests {
+
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://the-internet.herokuapp.com/");
+        System.out.println(driver.getTitle());
+        driver.quit();
+    }
+
+    public static void main(String[] args) {
+        BaseTests test = new BaseTests();
+        test.setUp();
+    }
+}
